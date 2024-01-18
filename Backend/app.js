b4fa2b65f52a04cfcf1bedd1bbb7bf7bfdd22d09
Mannat Jaiswal <mannatjaiswal03@gmail.com>
2024-01-18 10:47:16 +0530

    Added Gemini API

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = "Describe Ram Mandir inauguration like a disneyland";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Justice Link");
